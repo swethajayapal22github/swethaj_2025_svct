d5480466fa5670afc304256f5a25f15da4f1f5f8
swethajayapal22github <146517394+swethajayapal22github@users.noreply.github.com>
2024-01-22 11:47:13 +0530

    Add files via upload

---
+++ Day1_ques2_division.py
@@ -0,0 +1,61 @@
+Task
+The provided code stub reads two integers,  and , from STDIN.
+
+Add logic to print two lines. The first line should contain the result of integer division,  // . The second line should contain the result of float division,  / .
+
+No rounding or formatting is necessary.
+
+Example
+
+
+The result of the integer division .
+The result of the float division is .
+Print:
+
+0
+0.6
+Input Format
+
+The first line contains the first integer, .
+The second line contains the second integer, .
+
+Output Format
+
+Print the two lines as described above.
+
+Sample Input 0
+
+4
+3
+Sample Output 0
+
+1
+1.33333333333
+Contest ends in 11 hours
+Submissions: 81
+Max Score: 10
+Difficulty: Easy
+Rate This Challenge:
+
+    
+More
+ 
+1
+if __name__ == '__main__':
+2
+    a = int(input())
+3
+    b = int(input())
+
+
+
+
+OUTPUT:
+
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a // b)
+    print(a / b)
+
